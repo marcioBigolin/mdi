2afddeb8804feddae528c6417816abfc607e90d7
Marcio Bigolin <marcio.bigolinn@gmail.com>
2023-11-03 08:37:33 -0300

    hello world

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="MDI - Análise com IA", page_icon="🌍", layout="wide")
+st.subheader("Explore os dados utilizando Inteligência artificial")
+st.markdown("##")
+theme_plotly = None
