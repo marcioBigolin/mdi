8cbfd4f7ba56767323bc8e7add7b7aaab13efe02
Marcio Bigolin <marcio.bigolinn@gmail.com>
2023-11-03 08:43:38 -0300

    dfdsf

--- streamlit.py
+++ streamlit.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(page_title="MDI - Análise com IA", page_icon="🌍", layout="wide")
-st.subheader("Explore os dados utilizando Inteligência artificial")
+st.subheader("Explore os dados utilizando Inteligência artificial na nuvem")
 st.markdown("##")
 theme_plotly = None
